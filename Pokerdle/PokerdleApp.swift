f804347bf3d46b8922ffae1fdb13432a17b64c8e
SebastianMorado <sabby.morado@gmail.com>
2022-07-27 20:43:06 +0800

    Initial Commit

---
+++ Pokerdle/PokerdleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokerdleApp.swift
+//  Pokerdle
+//
+//  Created by Sebastian Morado on 7/27/22.
+//
+
+import SwiftUI
+
+@main
+struct PokerdleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
